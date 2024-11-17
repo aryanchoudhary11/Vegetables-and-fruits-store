49e4db1c73e97a796b52d5447a5e44c022ccd22b
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2024-11-17 22:18:53 +0530

    billing box added

--- bag.js
+++ bag.js
@@ -1,5 +1,3 @@
-
-
 let bagItemObjects;
 onload();
 function onload(){
@@ -8,7 +6,6 @@ function onload(){
 }
 
 function loadBagItemObjects(){
-    console.log(bag);
     bagItemObjects = bag.map(bagItem => {
         for(let i = 0; i < items.length; i++){
             if(bagItem.name == items[i].name){
@@ -16,12 +13,19 @@ function loadBagItemObjects(){
             }
         }
     });
-    console.log(bagItemObjects);
 
 }
 
 function displayBagItems() {
     let containerElement = document.querySelector('.main-container');
+    if (bagItemObjects.length === 0) {
+        containerElement.innerHTML = `
+        <div class="empty-bag">
+            <p>Your bag is Empty</p>
+        </div>`;
+        return;
+    }
+
     let innerHTML = '';
     bagItemObjects.forEach(bagItem => {
         innerHTML += generateHTML(bagItem);
@@ -39,48 +43,48 @@ function removeFromBag(itemName){
 }
 
 function generateHTML(item){
-    return `
-    <div class="boxes-container">
-        <div class="item-container" id="item">
-            <img src="${item.imagePath}" alt="${item.name}">
-        </div>
-        <div class="item-details">
-            <span id="name">${item.name}</span><br>
-            <span id="price">MRP: <s>${item.price}</s> ${item.discounted_price}</span>
-            <span id="discount">(${item.discount} OFF)</span><br>
-            <div class="buttons">
-                <div class="counter">
-                    <button class="decrease-btn">-</button>
-                    <span class="quantity">1</span><span><b>Kg</b></span>
-                    <button class="increase-btn">+</button>
-                </div>
-                <button id="remove-btn" onclick = "removeFromBag('${item.name}')">Remove</button>
+
+        return `
+        <div class="boxes-container">
+            <div class="item-container" id="item">
+                <img src="${item.imagePath}" alt="${item.name}">
             </div>
-        </div>
-    </div>`
+            <div class="item-details">
+                <span id="name">${item.name}</span><br>
+                <span id="price">MRP: <s>${item.price}</s> ${item.discounted_price}</span>
+                <span id="discount">(${item.discount} OFF)</span><br>
+                <div class="buttons">
+                    <div class="counter">
+                        <button class="decrease-btn">-</button>
+                        <span class="quantity">1</span><span><b>Kg</b></span>
+                        <button class="increase-btn">+</button>
+                    </div>
+                    <button id="remove-btn" onclick = "removeFromBag('${item.name}')">Remove</button>
+                </div>
+            </div>
+        </div>`
+
 }
 // Function to handle increase and decrease buttons
 
-    document.querySelectorAll('.boxes-container').forEach((box, index) => {
-        const decreaseBtn = box.querySelector('.decrease-btn');
-        const increaseBtn = box.querySelector('.increase-btn');
-        const quantityDisplay = box.querySelector('.quantity');
+document.querySelectorAll('.boxes-container').forEach((box) => {
+    const decreaseBtn = box.querySelector('.decrease-btn');
+    const increaseBtn = box.querySelector('.increase-btn');
+    const quantityDisplay = box.querySelector('.quantity');
 
-        let quantity = 1; // Initialize quantity for each item
-        // Set quantity text content for each item independently
+    let quantity = 1; 
+
+    quantityDisplay.textContent = quantity;
+
+    increaseBtn.addEventListener('click', () => {
+        quantity++;
         quantityDisplay.textContent = quantity;
-
-        // Increase quantity on clicking the increase button
-        increaseBtn.addEventListener('click', () => {
-            quantity++;
-            quantityDisplay.textContent = quantity;
-        });
-
-        // Decrease quantity on clicking the decrease button
-        decreaseBtn.addEventListener('click', () => {
-            if (quantity > 1) {
-                quantity--;
-            }
-            quantityDisplay.textContent = quantity;
-        });
     });
+
+    decreaseBtn.addEventListener('click', () => {
+        if (quantity > 1) {
+            quantity--;
+        }
+        quantityDisplay.textContent = quantity;
+    });
+});
