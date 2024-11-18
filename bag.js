bbc25839f6f9508740b31e8db6480a73066cb547
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2024-11-18 20:57:22 +0530

    billing container work done

--- bag.js
+++ bag.js
@@ -1,8 +1,16 @@
 let bagItemObjects;
+let totalPriceAns;
+let totalDiscount;
+let delivery;
 onload();
 function onload(){
     loadBagItemObjects();
     displayBagItems();
+    totalPriceAns = totalpriceFunction();
+    totalDiscount = discountFunction();
+    delivery = deliveryCharges();
+    totalBill = totalAmountFunction();
+    displayBilling();
 }
 
 function loadBagItemObjects(){
@@ -18,28 +26,32 @@ function loadBagItemObjects(){
 
 function displayBagItems() {
     let containerElement = document.querySelector('.main-container');
+    let bagContainer = document.querySelector('.bag-page');
     if (bagItemObjects.length === 0) {
-        containerElement.innerHTML = `
+        bagContainer.innerHTML = `
         <div class="empty-bag">
             <p>Your bag is Empty</p>
         </div>`;
         return;
     }
-
     let innerHTML = '';
     bagItemObjects.forEach(bagItem => {
         innerHTML += generateHTML(bagItem);
     });
     containerElement.innerHTML = innerHTML;
-
 }
 
 function removeFromBag(itemName){
     bag = bag.filter(bagItemName => bagItemName.name != itemName);
     localStorage.setItem('bag', JSON.stringify(bag));
     loadBagItemObjects();
+    totalPriceAns = totalpriceFunction();
+    totalDiscount = discountFunction();
+    delivery = deliveryCharges();
+    totalBill = totalAmountFunction();
     displayBagCount();
     displayBagItems();
+    displayBilling();
 }
 
 function generateHTML(item){
@@ -65,6 +77,67 @@ function generateHTML(item){
         </div>`
 
 }
+function totalpriceFunction(){
+    let totalPrice = 0;
+    bagItemObjects.forEach(bagItem => {
+        totalPrice += Number(bagItem.price);
+    });
+    return totalPrice.toFixed(2);
+}
+
+function discountFunction(){
+    let discount = 0;
+    bagItemObjects.forEach(bagItem => {
+        discount +=  Number(bagItem.price) - Number(bagItem.discounted_price);
+    });
+    return discount.toFixed(2);
+}
+
+function deliveryCharges(){
+    let charges = 0;
+    if(totalPriceAns < 400){
+        charges = 29;
+    }
+    return charges;
+}
+
+function totalAmountFunction(){
+    let totalAmount = 0;
+    totalAmount += totalPriceAns - totalDiscount + delivery;
+    return totalAmount.toFixed(2);
+}
+
+function displayBilling(){
+    let billingContainer = document.querySelector('.billing-container');
+    let innerBilling = `
+    <div class="billing-details">
+            <span><b>PRICE DETAILS (${bagItemObjects.length} items)</b> </span>
+            <div id="total-price">
+              <span>Total MRP</span>
+              <span>₹${totalPriceAns}</span>
+            </div>
+            <div id="total-discount">
+              <span>Discount </span>
+              <span id="discount-price">-₹${totalDiscount}</span>
+            </div>
+            <div id="Delievery-fees">
+              <span>Delivery charges <sub>(if applicable)</sub></span>
+              <span>₹${delivery}</span>
+            </div>
+          </div>
+          <div class="total-amount">
+            <span>Total Amount</span>
+            <span>₹${totalBill}</span>
+          </div>
+          <div class="place-order-btn">
+            <button>PLACE ORDER</button>
+          </div>
+    `;
+    billingContainer.innerHTML = innerBilling;
+}
+
+
+
 // Function to handle increase and decrease buttons
 
 document.querySelectorAll('.boxes-container').forEach((box) => {
