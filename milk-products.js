52886bffc22a3190d359a7a86502afafe9b014ed
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2024-11-19 13:02:04 +0530

    nuts and milk products added

---
+++ milk-products.js
@@ -0,0 +1,42 @@
+
+let menuIcon = document.querySelector('#menu-box');
+let navbar = document.querySelector('#nav_bar');
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+
+let bag;
+onLoad();
+function onLoad(){
+    let bagItems = localStorage.getItem('bag');
+    bag = bagItems ? JSON.parse(bagItems) : [];
+    displayBagCount();
+}
+function addToBag(event) {
+    const fruitsBox = event.target.closest('.milk-products-box');
+    const name = fruitsBox.querySelector('#name').textContent;
+    const price = fruitsBox.querySelector('#price').textContent;
+    const discount = fruitsBox.querySelector('#discount').textContent;
+    const item = { name, price, discount };
+    bag.push(item);
+    localStorage.setItem('bag', JSON.stringify(bag));
+    displayBagCount();
+}
+document.querySelectorAll('.bag-btn button').forEach(button => {
+    button.addEventListener('click', addToBag);
+    
+});
+
+function displayBagCount(){
+    let elementCount = document.querySelector('.items-count');
+    if(bag.length > 0){
+        elementCount.style.visibility = 'visible';
+        elementCount.innerText = bag.length;
+    } else{
+        elementCount.style.visibility = 'hidden';
+    }
+}
